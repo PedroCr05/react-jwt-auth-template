8fae1efc34f7f1edba173f1ae9918a0ebbd0d671
pedrocruz <JayJMarquez05@gmail.com>
2024-11-13 13:51:54 -0600

    Sign out does work now

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
+import * as authService from "../src/services/authService.js";
 import Dashboard from "./components/DashBoard/DashBoard";
 import Landing from "./components/Landing/Landing";
 import NavBar from "./components/NavBar/NavBar";
 import SignInForm from "./components/SignInForm/SignInForm";
 import SignUpForm from "./components/SignUpForm/SignUpForm";
-import * as authService from "../src/services/authService.js";
 
 const App = () => {
   // To test this state. You can put a truthy value to get the opposite result
   const [user, setUser] = useState(authService.getUser());
 
+  const handleSignout = () => {
+    authService.signout();
+    setUser(null);
+  };
+
   return (
     <>
-      <NavBar user={user} />
+      <NavBar user={user} handleSignout={handleSignout} />
       <Routes>
         {user ? (
           <Route path="/" element={<Dashboard user={user} />} />
